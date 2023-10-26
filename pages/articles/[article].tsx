bf1a0603509403a7e282438c4664a725c1887ae6
knightlore <gnightshade@gmail.com>
2023-10-26 14:41:22 -0300

    Add Algolia searchs

--- pages/articles/[article].tsx
+++ pages/articles/[article].tsx
@@ -1,74 +1,38 @@
-import { extractMetaTags } from '@ircsignpost/signpost-base/dist/src/article-content';
-import {
-  ArticlePageStrings,
-  getErrorResponseProps,
-} from '@ircsignpost/signpost-base/dist/src/article-page';
-import ArticlePage, {
-  MountArticle,
-} from '@ircsignpost/signpost-base/dist/src/article-page';
-import CookieBanner from '@ircsignpost/signpost-base/dist/src/cookie-banner';
-import Footer from '@ircsignpost/signpost-base/dist/src/footer';
-import { MenuOverlayItem } from '@ircsignpost/signpost-base/dist/src/menu-overlay';
-import { createDefaultSearchBarProps } from '@ircsignpost/signpost-base/dist/src/search-bar';
-import {
-  CategoryWithSections,
-  ZendeskCategory,
-  getArticle,
-  getArticles,
-  getCategories,
-  getCategoriesWithSections,
-  getTranslationsFromDynamicContent,
-} from '@ircsignpost/signpost-base/dist/src/zendesk';
-import { GetStaticProps } from 'next';
-import getConfig from 'next/config';
-import { useRouter } from 'next/router';
-import React from 'react';
-
-import {
-  ABOUT_US_ARTICLE_ID,
-  CATEGORIES_TO_HIDE,
-  CATEGORY_ICON_NAMES,
-  GOOGLE_ANALYTICS_IDS,
-  REVALIDATION_TIMEOUT_SECONDS,
-  SEARCH_BAR_INDEX,
-  SECTION_ICON_NAMES,
-  SITE_TITLE,
-  USE_CAT_SEC_ART_CONTENT_STRUCTURE,
-  ZENDESK_AUTH_HEADER,
-} from '../../lib/constants';
-import {
-  LOCALES,
-  LOCALE_CODES_TO_CANONICAL_LOCALE_CODES,
-  Locale,
-  getLocaleFromCode,
-  getZendeskLocaleId,
-} from '../../lib/locale';
-import { getHeaderLogoProps } from '../../lib/logo';
-import { getFooterItems, getMenuItems } from '../../lib/menu';
-import {
-  COMMON_DYNAMIC_CONTENT_PLACEHOLDERS,
-  ERROR_DYNAMIC_CONTENT_PLACEHOLDERS,
-  generateArticleErrorProps,
-  populateArticlePageStrings,
-  populateMenuOverlayStrings,
-} from '../../lib/translations';
-import { getSiteUrl, getZendeskMappedUrl, getZendeskUrl } from '../../lib/url';
+import { extractMetaTags } from '@ircsignpost/signpost-base/dist/src/article-content'
+import { ArticlePageStrings, getErrorResponseProps, } from '@ircsignpost/signpost-base/dist/src/article-page'
+import ArticlePage, { MountArticle, } from '@ircsignpost/signpost-base/dist/src/article-page'
+import CookieBanner from '@ircsignpost/signpost-base/dist/src/cookie-banner'
+import Footer from '@ircsignpost/signpost-base/dist/src/footer'
+import { MenuOverlayItem } from '@ircsignpost/signpost-base/dist/src/menu-overlay'
+import { createDefaultSearchBarProps } from '@ircsignpost/signpost-base/dist/src/search-bar'
+import { CategoryWithSections, ZendeskCategory, getArticle, getArticles, getCategories, getCategoriesWithSections, getTranslationsFromDynamicContent, } from '@ircsignpost/signpost-base/dist/src/zendesk'
+import { GetStaticProps } from 'next'
+import getConfig from 'next/config'
+import { useRouter } from 'next/router'
+import React from 'react'
+import { ABOUT_US_ARTICLE_ID, ALGOLIA_ARTICLE_INDEX_NAME, ALGOLIA_QUERY_INDEX_NAME, ALGOLIA_SEARCH_API_KEY_WRITE, ALGOLIA_SEARCH_APP_ID, CATEGORIES_TO_HIDE, CATEGORY_ICON_NAMES, GOOGLE_ANALYTICS_IDS, REVALIDATION_TIMEOUT_SECONDS, SEARCH_BAR_INDEX, SECTION_ICON_NAMES, SITE_TITLE, USE_CAT_SEC_ART_CONTENT_STRUCTURE, ZENDESK_AUTH_HEADER, } from '../../lib/constants'
+import { LOCALES, LOCALE_CODES_TO_CANONICAL_LOCALE_CODES, Locale, getLocaleFromCode, getZendeskLocaleId, } from '../../lib/locale'
+import { getHeaderLogoProps } from '../../lib/logo'
+import { getFooterItems, getMenuItems } from '../../lib/menu'
+import { COMMON_DYNAMIC_CONTENT_PLACEHOLDERS, ERROR_DYNAMIC_CONTENT_PLACEHOLDERS, generateArticleErrorProps, populateArticlePageStrings, populateMenuOverlayStrings, } from '../../lib/translations'
+import { getSiteUrl, getZendeskMappedUrl, getZendeskUrl } from '../../lib/url'
+import algoliasearch from "algoliasearch/lite"
 
 interface ArticleProps {
-  pageTitle: string;
-  articleTitle: string;
-  articleContent: string;
-  metaTagAttributes: object[];
-  siteUrl: string;
-  articleId: number;
-  lastEditedValue: string;
-  locale: Locale;
-  pageUnderConstruction?: boolean;
-  preview: boolean;
-  strings: ArticlePageStrings;
+  pageTitle: string
+  articleTitle: string
+  articleContent: string
+  metaTagAttributes: object[]
+  siteUrl: string
+  articleId: number
+  lastEditedValue: string
+  locale: Locale
+  pageUnderConstruction?: boolean
+  preview: boolean
+  strings: ArticlePageStrings
   // A list of |MenuOverlayItem|s to be displayed in the header and side menu.
-  menuOverlayItems: MenuOverlayItem[];
-  footerLinks?: MenuOverlayItem[];
+  menuOverlayItems: MenuOverlayItem[]
+  footerLinks?: MenuOverlayItem[]
 }
 
 export default function Article({
@@ -86,8 +50,8 @@ export default function Article({
   menuOverlayItems,
   footerLinks,
 }: ArticleProps) {
-  const router = useRouter();
-  const { publicRuntimeConfig } = getConfig();
+  const router = useRouter()
+  const { publicRuntimeConfig } = getConfig()
 
   return (
     <ArticlePage
@@ -144,7 +108,7 @@ export default function Article({
         }}
       />
     </ArticlePage>
-  );
+  )
 }
 
 async function getStaticParams() {
@@ -152,45 +116,43 @@ async function getStaticParams() {
     Object.values(LOCALES).map(
       async (locale) => await getArticles(locale, getZendeskUrl())
     )
-  );
+  )
 
   return articles.flat().map((article) => {
     return {
       article: article.id.toString(),
       locale: article.locale,
-    };
-  });
+    }
+  })
 }
 
 export async function getStaticPaths() {
-  const articleParams = await getStaticParams();
+  const articleParams = await getStaticParams()
 
   return {
     paths: articleParams.map(({ article, locale }) => {
       return {
         params: { article },
         locale,
-      };
+      }
     }),
     fallback: 'blocking',
-  };
+  }
 }
 
 function getStringPath(article: string, locale: string): string {
-  return `/${locale}/articles/${article}`;
+  return `/${locale}/articles/${article}`
 }
 
 export async function getStringPaths(): Promise<string[]> {
-  const params = await getStaticParams();
-  return params.map((param) => getStringPath(param.article, param.locale));
+  const params = await getStaticParams()
+  return params.map((param) => getStringPath(param.article, param.locale))
 }
 
-export const getStaticProps: GetStaticProps = async ({
-  params,
-  locale,
-  preview,
-}) => {
-  const currentLocale = getLocaleFromCode(locale ?? '');
+export const getStaticProps: GetStaticProps = async ({ params, locale, preview, }) => {
+
+  const currentLocale = getLocaleFromCode(locale ?? '')
+
   let dynamicContent = await getTranslationsFromDynamicContent(
     getZendeskLocaleId(currentLocale),
     [
@@ -199,26 +161,26 @@ export const getStaticProps: GetStaticProps = async ({
     ],
     getZendeskUrl(),
     ZENDESK_AUTH_HEADER
-  );
+  )
 
-  let categories: ZendeskCategory[] | CategoryWithSections[];
+  let categories: ZendeskCategory[] | CategoryWithSections[]
   if (USE_CAT_SEC_ART_CONTENT_STRUCTURE) {
     categories = await getCategoriesWithSections(
       currentLocale,
       getZendeskUrl(),
       (c) => !CATEGORIES_TO_HIDE.includes(c.id)
-    );
+    )
     categories.forEach(({ sections }) => {
       sections.forEach(
         (s) => (s.icon = SECTION_ICON_NAMES[s.id] || 'help_outline')
-      );
-    });
+      )
+    })
   } else {
-    categories = await getCategories(currentLocale, getZendeskUrl());
-    categories = categories.filter((c) => !CATEGORIES_TO_HIDE.includes(c.id));
+    categories = await getCategories(currentLocale, getZendeskUrl())
+    categories = categories.filter((c) => !CATEGORIES_TO_HIDE.includes(c.id))
     categories.forEach(
       (c) => (c.icon = CATEGORY_ICON_NAMES[c.id] || 'help_outline')
-    );
+    )
   }
   const aboutUsArticle = await getArticle(
     currentLocale,
@@ -226,19 +188,19 @@ export const getStaticProps: GetStaticProps = async ({
     getZendeskUrl(),
     getZendeskMappedUrl(),
     ZENDESK_AUTH_HEADER
-  );
+  )
   const menuOverlayItems = getMenuItems(
     populateMenuOverlayStrings(dynamicContent),
     categories,
     !!aboutUsArticle
-  );
+  )
 
-  const strings = populateArticlePageStrings(dynamicContent);
+  const strings = populateArticlePageStrings(dynamicContent)
 
   const footerLinks = getFooterItems(
     populateMenuOverlayStrings(dynamicContent),
     categories
-  );
+  )
 
   const article = await getArticle(
     currentLocale,
@@ -247,7 +209,7 @@ export const getStaticProps: GetStaticProps = async ({
     getZendeskMappedUrl(),
     ZENDESK_AUTH_HEADER,
     preview ?? false
-  );
+  )
 
   // If article does not exist, return an error.
   if (!article) {
@@ -261,33 +223,52 @@ export const getStaticProps: GetStaticProps = async ({
         mappedUrl: getZendeskMappedUrl(),
         authHeader: ZENDESK_AUTH_HEADER,
       }
-    );
+    )
 
-    const subtitle = generateArticleErrorProps(dynamicContent).subtitle ?? '';
+    const subtitle = generateArticleErrorProps(dynamicContent).subtitle ?? ''
     return errorProps.notFound
       ? // When the error is notFound, router automatically redirects
-        // to 404 page which has its own logic for fetching props.
-        errorProps
+      // to 404 page which has its own logic for fetching props.
+      errorProps
       : // For other error types we have custom error handling logic inside
-        // ArticlePage component, so we pass ArticlePage's and error props.
-        {
-          props: {
-            ...errorProps,
-            pageTitle: `${subtitle} - ${SITE_TITLE}`,
-            strings,
-            menuOverlayItems,
-            siteUrl: getSiteUrl(),
-            articleId: Number(params?.article),
-            locale: currentLocale,
-            preview: preview ?? false,
-            metaTagAttributes: [],
-          },
-        };
+      // ArticlePage component, so we pass ArticlePage's and error props.
+      {
+        props: {
+          ...errorProps,
+          pageTitle: `${subtitle} - ${SITE_TITLE}`,
+          strings,
+          menuOverlayItems,
+          siteUrl: getSiteUrl(),
+          articleId: Number(params?.article),
+          locale: currentLocale,
+          preview: preview ?? false,
+          metaTagAttributes: [],
+        },
+      }
   }
 
-  const [metaTagAttributes, content] = article.body
-    ? extractMetaTags(article.body)
-    : [[], article.body];
+
+
+  const [metaTagAttributes, content] = article.body ? extractMetaTags(article.body) : [[], article.body]
+
+
+  if (article) {
+    //Create algolia index
+    const content = stripHtmlTags(article.body || "")
+    const name = article.title || ""
+    const query = name
+    const objectID = article.id
+    try {
+      const client = algoliasearch(ALGOLIA_SEARCH_APP_ID, ALGOLIA_SEARCH_API_KEY_WRITE)
+      const record = { objectID, name, content }
+      const index: any = client.initIndex(ALGOLIA_ARTICLE_INDEX_NAME)
+      await index.saveObject(record)
+      const indexquery: any = client.initIndex(ALGOLIA_QUERY_INDEX_NAME)
+      await indexquery.saveObject({ objectID, name, content, query })
+    } catch (error) {
+      console.error(`Error creating algolia index: ${error?.toString() ?? "Uknown error"}`)
+    }
+  }
 
   return {
     props: {
@@ -305,5 +286,11 @@ export const getStaticProps: GetStaticProps = async ({
       footerLinks,
     },
     revalidate: REVALIDATION_TIMEOUT_SECONDS,
-  };
-};
+  }
+}
+
+
+function stripHtmlTags(html: string): string {
+  const regex = /<[^>]*>|&[^;]+;|<img\s+.*?>|<span\s+style="[^"]*">.*?<\/span>|&[A-Za-z]+;/g
+  return html.replace(regex, '')
+}
