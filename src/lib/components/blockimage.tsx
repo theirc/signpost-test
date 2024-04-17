02b11bfececab2ee4c9171fce3d9c2035d898a4b
Bianca Chami <b.chami39@gmail.com>
2024-04-17 11:10:51 +0100

    feat: image

--- src/lib/components/blockimage.tsx
+++ src/lib/components/blockimage.tsx
@@ -4,8 +4,11 @@ import { Container } from "./container"
 export function BlockImage(props: { block: BlockImage }) {
   const { block } = props
 
+  const imageUrl = `https://signpost.azureedge.net/${translate(block.image)}`;
+
   return <Container block={block}>
-    <div>Image: {translate(props.block.image)} </div>
+    {/* <img src={imageUrl} alt="block-image" /> */}
+    <img src="https://signpost.azureedge.net/0016e070-29b4-4a69-a9ea-5e0e992dead1.png" style={{maxWidth: '100%', height: 'auto'}} />
   </Container>
 
 }
