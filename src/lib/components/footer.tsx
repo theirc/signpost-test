cb441d8b01ad210ab397d46a0c991cfb48c83c4f
Bianca Chami <b.chami39@gmail.com>
2024-06-26 14:36:30 -0400

    footer styling

--- src/lib/components/footer.tsx
+++ src/lib/components/footer.tsx
@@ -5,15 +5,24 @@ import { Container } from "./container";
 export function Footer() {
   return (
     <Container block={app.page.footer}>
-      <div className="mb-16">{translate(app.page.footer.text)}</div>
-      <div className="mb-16">
-        {app.page.footer?.footerlinks.map((link) => {
+      <div className="w-full flex flex-col">
+
+      <Link to="/" className="mb-20">
+        <img src={app.logo} height={30} alt="Logo"/>
+      </Link>
+
+      <div className=" mb-5 flex flex-wrap">
+     {app.page.footer?.footerlinks.map((link) => {
           return (
-            <Link key={`${link.title}-${link.url}`} to={link.url} className="mx-1">
+            <Link key={`${link.title}-${link.url}`} to={link.url} className="items-start mr-4 text-white">
               {translate(link.title)}
             </Link>
           );
         })}
+      </div>
+    
+      <div className="mb-5">{translate(app.page.footer.text)}</div>
+   
       </div>
     </Container>
   );
