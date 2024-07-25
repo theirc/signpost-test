a49d7a17006eff682b3e61d1120315dd766be747
Bianca Chami <b.chami39@gmail.com>
2024-07-25 15:35:32 -0400

    feat: fixed file

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     preserveSymlinks: true
-  },
-  server: {
-    fs: {
-      allow: ['..']
-    }
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
-  assetsInclude: ['**/*.json'],
+  }
 })
