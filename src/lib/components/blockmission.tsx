0761454f773162f2c4908cf9b11e8b4712ab6888
Bianca Chami <b.chami39@gmail.com>
2024-08-12 10:47:08 -0400

    feat: fixed mission block

--- src/lib/components/blockmission.tsx
+++ src/lib/components/blockmission.tsx
@@ -1,8 +1,9 @@
 import { CSSProperties } from "react"
 import { useNavigate } from "react-router-dom";
+import { languages } from "../locale"
 import { Button} from "antd";
 import { SearchOutlined } from "@ant-design/icons";
-import { translate } from "../app"
+import { app, translate } from "../app"
 import { Container } from "./container"
 import Search from "antd/es/input/Search";
 import { translations } from "../../translations";
@@ -11,6 +12,7 @@ import { useAnimateOnScroll } from "./useAnimateOnScroll";
 export function BlockMission(props: { block: BlockMission}) {
   const { block } = props
   const navigate = useNavigate();
+  const isRTL = languages[app.locale]?.rtl;
   useAnimateOnScroll();
 
 
@@ -20,17 +22,17 @@ export function BlockMission(props: { block: BlockMission}) {
 
   return <Container block={block}>
     <div className="py-12 px-4 max-w-4xl mx-auto container-center">
-    <h1 className="fade-up-0 text-3xl md:text-4xl text-center max-w-xl mission-heading"           data-animation="animate__fadeInUp">{translate(block.title)}</h1>
-   
- 
-    <Search
-   placeholder= {translate(translations.searchForInformation)}
-   enterButton={<Button type="primary" icon={<SearchOutlined />}>{translate(translations.search)}</Button>}
-   size="large"
-   onSearch={handleSearch}
-   className="fade-up-1 header-search-input"
-   data-animation="animate__fadeInUp"
-   />
+    <h1 className="fade-up-0 text-3xl md:text-4xl text-center max-w-xl mission-heading"           
+    data-animation="animate__fadeInUp">{translate(block.title)}</h1>
+    <div className={`header-search-input fade-up-1 ${isRTL ? 'rtl' : ''}`}
+  data-animation="animate__fadeInUp">
+ <Search
+  placeholder={translate(translations.searchForInformation)}
+  enterButton={<Button type="primary" icon={<SearchOutlined />}>{translate(translations.search)}</Button>}
+  size="large"
+  onSearch={handleSearch}
+/>
+</div>
    </div>
   </Container>
 } 
