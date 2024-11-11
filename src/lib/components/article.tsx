b3971f3f20248fe43e608041b4db8e33bf0fc109
Emilio Guisolfo <emilioguisolfo@gmail.com>
2024-11-11 14:47:46 -0300

    fix readspeaker (#125)

--- src/lib/components/article.tsx
+++ src/lib/components/article.tsx
@@ -49,36 +49,36 @@ export function Article() {
   }
 
   return <div className={`overflow-y-auto ${isRTL ? 'rtl' : ''}`}>
-    <Container className="text-black bg-white">
+    <Container className="text-black bg-white" id="main-content">
       <div className={`flex flex-col ${isRTL ? 'text-right' : ''}`} ref={refScrollUp}>
-      <Breadcrumb separator=">" items={[{title: <a href="/">{translate(translations.home)}</a>}, {title: <a href={`/${locale.toLowerCase()}/categories/${category.id}`}>{translate(category.name)}</a>}, {title: <a href={`/${locale.toLowerCase()}/categories/${category.id}/${section.id}`}>{translate(section.name)}</a>}, {title: translate(a.name)}]} />
+        <Breadcrumb className="rs_skip" separator=">" items={[{ title: <a href="/">{translate(translations.home)}</a> }, { title: <a href={`/${locale.toLowerCase()}/categories/${category.id}`}>{translate(category.name)}</a> }, { title: <a href={`/${locale.toLowerCase()}/categories/${category.id}/${section.id}`}>{translate(section.name)}</a> }, { title: translate(a.name) }]} />
         <div>
           <h1 className="fade-up-0 text-4xl font-medium leading-snug"
-              data-animation="animate__fadeInUp">{title}</h1>
+            data-animation="animate__fadeInUp">{title}</h1>
           <p className="fade-up-1 text-sm font-light leading-tight"
-              data-animation="animate__fadeInUp">{new Date(a.updated_at).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</p>
+            data-animation="animate__fadeInUp">{new Date(a.updated_at).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</p>
           <div className={`flex items-center mt-4 ${isRTL ? 'flex-row-reverse' : ''}`}>
             <TextReader />
             <ShareButton />
           </div>
         </div>
-        {a && <div className={`article mt-10 ${isRTL ? 'text-right' : 'text-left'} fade-up-2`}dangerouslySetInnerHTML={{ __html: body }} data-animation="animate__fadeInUp" />}
+        {a && <div className={`article mt-10 ${isRTL ? 'text-right' : 'text-left'} fade-up-2`} dangerouslySetInnerHTML={{ __html: body }} data-animation="animate__fadeInUp" />}
         <div className="relative w-full mt-8">
-    <a 
-      onClick={handleScrollToTop} 
-      className={`
+          <a
+            onClick={handleScrollToTop}
+            className={`
         absolute underline cursor-pointer
         ${isRTL ? 'left-0' : 'right-0'}
       `}
-    >
-      <strong className={`flex items-center text-base font-medium ${isRTL ? 'flex-row-reverse' : ''}`}>
-        {translate(translations.backToTop)} 
-        <ArrowUpOutlined className={isRTL ? 'mr-2' : 'ml-2'} />
-      </strong>
-    </a>
-  </div>
+          >
+            <strong className={`flex items-center text-base font-medium ${isRTL ? 'flex-row-reverse' : ''}`}>
+              {translate(translations.backToTop)}
+              <ArrowUpOutlined className={isRTL ? 'mr-2' : 'ml-2'} />
+            </strong>
+          </a>
+        </div>
       </div>
-      </Container>
+    </Container>
     <div className="py-16 w-full md:flex justify-center text-black bg-[#FFF8F7] h-auto hidden">
       <div className="sm:w-full px-8 lg:w-4/5 w-screen">
         {channels.title && (
@@ -89,55 +89,55 @@ export function Article() {
         )}
         <div className="flex flex-col md:flex-row flex-wrap gap-2 md:gap-4 justify-center md:justify-between max-w-sm md:max-w-full mx-auto md:p-4">
           {channels.fb_link && (
-        <a
-        href={translate(channels.fb_link)}
-        className="fade-up-3 social-icon"
-        data-animation="animate__fadeInUp"
-        target="_blank"
-        aria-label="Facebook"
-      >
-        <FaFacebook className="social-icon-svg" />
-        <div className="social-icon-text">{translate(channels.fb)}</div>
-      </a>
+            <a
+              href={translate(channels.fb_link)}
+              className="fade-up-3 social-icon"
+              data-animation="animate__fadeInUp"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <FaFacebook className="social-icon-svg" />
+              <div className="social-icon-text">{translate(channels.fb)}</div>
+            </a>
           )}
           {channels.fbmess_link && (
-           <a
-           href={translate(channels.fbmess_link)}
-           className="fade-up-3 social-icon"
-           data-animation="animate__fadeInUp"
-           target="_blank"
-           aria-label="Facebook Messenger"
-         >
-           <FaFacebookMessenger className="social-icon-svg"  />
-           <div className="social-icon-text">{translate(channels.fbmess)}</div>
-         </a>
+            <a
+              href={translate(channels.fbmess_link)}
+              className="fade-up-3 social-icon"
+              data-animation="animate__fadeInUp"
+              target="_blank"
+              aria-label="Facebook Messenger"
+            >
+              <FaFacebookMessenger className="social-icon-svg" />
+              <div className="social-icon-text">{translate(channels.fbmess)}</div>
+            </a>
           )}
           {channels.whatsapp_link && (
-             <a
-             href={translate(channels.whatsapp_link)}
-             className="fade-up-3 social-icon"
-             data-animation="animate__fadeInUp"
-             target="_blank"
-             aria-label="Whatsapp"
-           >
-             <FaWhatsapp className="social-icon-svg"/>
-             <div className="social-icon-text">{translate(channels.whatsapp)}</div>
-           </a>
+            <a
+              href={translate(channels.whatsapp_link)}
+              className="fade-up-3 social-icon"
+              data-animation="animate__fadeInUp"
+              target="_blank"
+              aria-label="Whatsapp"
+            >
+              <FaWhatsapp className="social-icon-svg" />
+              <div className="social-icon-text">{translate(channels.whatsapp)}</div>
+            </a>
           )}
           {channels.email_link && (
-          <a
-          href={`mailto:${translate(channels.email_link)}`}
-          className="fade-up-3 social-icon"
-          data-animation="animate__fadeInUp"
-          target="_blank"
-          aria-label="Email"
-        >
-          <FaEnvelope className="social-icon-svg"/>
-          <div className="social-icon-text">{translate(channels.email)}</div>
-        </a>
+            <a
+              href={`mailto:${translate(channels.email_link)}`}
+              className="fade-up-3 social-icon"
+              data-animation="animate__fadeInUp"
+              target="_blank"
+              aria-label="Email"
+            >
+              <FaEnvelope className="social-icon-svg" />
+              <div className="social-icon-text">{translate(channels.email)}</div>
+            </a>
           )}
           {channels.instagram_link && (
-              <a
+            <a
               href={translate(channels.instagram_link)}
               className="fade-up-3 social-icon"
               data-animation="animate__fadeInUp"
@@ -149,40 +149,40 @@ export function Article() {
             </a>
           )}
           {channels.tiktok_link && (
-           <a
-           href={translate(channels.tiktok_link)}
-           className="fade-up-3 social-icon"
-           data-animation="animate__fadeInUp"
-           target="_blank"
-           aria-label="TikTok"
-         >
-           <FaTiktok className="social-icon-svg" />
-           <div className="social-icon-text">{translate(channels.tiktok)}</div>
-         </a>
+            <a
+              href={translate(channels.tiktok_link)}
+              className="fade-up-3 social-icon"
+              data-animation="animate__fadeInUp"
+              target="_blank"
+              aria-label="TikTok"
+            >
+              <FaTiktok className="social-icon-svg" />
+              <div className="social-icon-text">{translate(channels.tiktok)}</div>
+            </a>
           )}
           {channels.telegram_link && (
-          <a
-          href={translate(channels.telegram_link)}
-          className="fade-up-3 social-icon"
-          data-animation="animate__fadeInUp"
-          target="_blank"
-          aria-label="Telegram"
-        >
-          <FaTelegram className="social-icon-svg" />
-          <div className="social-icon-text">{translate(channels.telegram)}</div>
-        </a>
+            <a
+              href={translate(channels.telegram_link)}
+              className="fade-up-3 social-icon"
+              data-animation="animate__fadeInUp"
+              target="_blank"
+              aria-label="Telegram"
+            >
+              <FaTelegram className="social-icon-svg" />
+              <div className="social-icon-text">{translate(channels.telegram)}</div>
+            </a>
           )}
           {channels.whatsappc_link && (
-             <a
-             href={translate(channels.whatsappc_link)}
-             className="fade-up-3 social-icon"
-             data-animation="animate__fadeInUp"
-             target="_blank"
-             aria-label="WhatsApp Channel"
-           >
-             <FaWhatsappSquare className="social-icon-svg" />
-             <div className="social-icon-text">{translate(channels.whatsappc)}</div>
-           </a>
+            <a
+              href={translate(channels.whatsappc_link)}
+              className="fade-up-3 social-icon"
+              data-animation="animate__fadeInUp"
+              target="_blank"
+              aria-label="WhatsApp Channel"
+            >
+              <FaWhatsappSquare className="social-icon-svg" />
+              <div className="social-icon-text">{translate(channels.whatsappc)}</div>
+            </a>
           )}
         </div>
       </div>
