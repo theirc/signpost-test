b188e026fdca3410f0d26ab664e2eb786a9df5bf
bgSignpost <brian.greenberg@rescue.org>
2025-05-16 17:38:18 -0400

    backwards compatibility

--- src/pages/knowledge.tsx
+++ src/pages/knowledge.tsx
@@ -2,3 +2,6 @@ import Knowledge from "./knowledge/index"
 
 // Re-export the Knowledge component as default
 export default Knowledge
+
+// Re-export the CollectionsManagement component for backward compatibility
+export { default as CollectionsManagement } from "./knowledge/index"
