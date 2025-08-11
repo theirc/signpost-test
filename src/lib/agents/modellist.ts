2b010bea8aa52dd22fac615a12c3d0a88267b0c0
bgSignpost <brian.greenberg@rescue.org>
2025-08-11 15:50:11 -0400

    add GPT5

--- src/lib/agents/modellist.ts
+++ src/lib/agents/modellist.ts
@@ -4,6 +4,9 @@ declare global {
 }
 
 const openAiModels = [
+  'gpt-5',
+  'gpt-5-mini',
+  'gpt-5-nano',
   'gpt-4.1',
   'gpt-4.1-mini',
   'gpt-4.1-nano',
@@ -59,4 +62,3 @@ export const AllAIModels = [
   ...ClaudeModels,
   ...GoogleModels,
 ]
-
