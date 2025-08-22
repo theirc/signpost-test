c7775c70562bc04779158a950b1243bfd802fa3b
Emilio Guisolfo <emilioguisolfo@gmail.com>
2025-08-22 19:32:30 -0300

    Dev emilio (#321)
    * fixed broken links
    
    * minor styles changes to docs

--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -11,18 +11,15 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        Signpost AI
+        <img
+          src="/images/screenshots/logo/signpost-ai-logo.png"
+          alt="Signpost AI Logo"
+          width="120"
+          height="120"
+          className="rounded"
+        />
       </>
     ),
   },
-  // see https://fumadocs.dev/docs/ui/navigation/links
   links: [],
 };
